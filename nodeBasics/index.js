6d610b335c8c308e24738a59a215b79b6725222c
Kate Konovalova <kate.konovalova.qa@gmail.com>
2025-07-08 21:36:59 +0300

    Home work 2: Intro JS

---
+++ nodeBasics/index.js
@@ -0,0 +1,5 @@
+import chalk from 'chalk';
+
+console.log(chalk.blue('Привіт з кольорової консолі!'));
+console.log(chalk.bgYellow.black('Це чорний текст на жовтому фоні'));
+console.log(chalk.red.bold('Помилка: щось пішло не так'));
