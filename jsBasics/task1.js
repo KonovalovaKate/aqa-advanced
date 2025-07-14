ee681e59b900a804d853920c39407bd0fde89fd8
Kate Konovalova <kate.konovalova.qa@gmail.com>
2025-07-14 12:20:22 +0300

    Homework 4: JavaScript primitive data types

---
+++ jsBasics/task1.js
@@ -0,0 +1,10 @@
+let myString = "Привіт, світ!";
+console.log("Рядок:", myString);
+let myNumber = 42;
+console.log("Число:", myNumber);
+let myBoolean = true;
+console.log("Булеве значення:", myBoolean);
+let myNull = null;
+console.log("Null:", myNull);
+let myUndefined;
+console.log("Undefined:", myUndefined);
